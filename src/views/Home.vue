c52d76b8fa1f1490105a118e5d32acc5cec26ca4
mateo1423 <75327637+mateo1423@users.noreply.github.com>
2021-01-09 18:07:59 +0100

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <div class="home" >
+    
+ <div class="row">
+ <div class="col-2"><!--useless--></div>
+ <div class="col-8">
+   <center>
+ <welcomebar />
+
+<div id="nextbutton">
+<router-link to="/computertype">Next</router-link></div>
+
+</center>
+ </div>
+ <div class="col-2">
+ <!--dont matter-->
+ </div>
+ </div>
+ </div>
+ </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Welcomebar from '../components/welcomebar.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Welcomebar
+   
   }
 }
-</script>
+</script >
+
+<style scoped lang="scss">
+#nextbutton {
+ background: #0978b8; 
+  text-align: center;
+  font-color: black;
+  font-weight: bold;
+  font-size: 200%;
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+  </style>
